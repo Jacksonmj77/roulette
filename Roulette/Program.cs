7ef54cb890a1b37d5a7ba152a11a30e78a66097e
Jacksonmj77 <60822114+Jacksonmj77@users.noreply.github.com>
2020-03-07 18:22:37 -0500

    design

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -7,7 +7,6 @@ namespace Roulette
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
-
             Console.WriteLine("   :::::::::       ::::::::      :::    :::       :::        ::::::::::   :::::::::::   :::::::::::       ::::::::::  ");
             Console.WriteLine("  :+:    :+:     :+:    :+:     :+:    :+:       :+:        :+:              :+:           :+:           :+:      ");
             Console.WriteLine(" +:+    +:+     +:+    +:+     +:+    +:+       +:+        +:+              +:+           +:+           +:+ ");
@@ -17,16 +16,37 @@ namespace Roulette
             Console.WriteLine("###    ###      ########       ########        ########## ##########       ###           ###           ##########   ");
             Console.ResetColor();
             Bin bin = Roll.Play();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Bets.BetOne(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Bets.BetTwo(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Bets.BetThree(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Bets.BetFour(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Bets.BetFive(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Bets.BetSix(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Bets.BetSeven(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Bets.BetEight(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Bets.BetNine(bin);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Bets.BetTen(bin);
+            Console.ResetColor();
+
 
         }
     }
